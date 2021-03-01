752f939ed3631f91ec3a6084495b77640bdab3e2
Moonseer12 <79872729+Moonseer12@users.noreply.github.com>
2021-03-01 14:26:42 -0700

    Update AncientMandible.cs

--- Items/AncientMandible.cs
+++ Items/AncientMandible.cs
@@ -7,17 +7,17 @@ namespace EnduriumMod.Items
         public override void SetDefaults()
         {
 
-            item.width = 50;
-            item.height = 57;
-            item.maxStack = 99;
+            item.width = 16;
+            item.height = 32;
+            item.maxStack = 999;
             item.value = Terraria.Item.sellPrice(0, 0, 5, 0);
             item.rare = 1;
         }
 
         public override void SetStaticDefaults()
         {
-            DisplayName.SetDefault("Primeval Mandible");
-            Tooltip.SetDefault("The ancient mandible");
+            DisplayName.SetDefault("Dunefang");
+            Tooltip.SetDefault("A fang of an Amber Warden\nAmber Wardens use these to dig through the sands, hunting for robbers and thieves");
         }
     }
 }
